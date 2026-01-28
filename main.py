0b63778659c1d5e49c05f5d8e1ae7e7a37574229
kuma <kumasan00m@gmail.com>
2026-01-28 17:02:20 +0900

    ppmからpngを生成するように変更

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import os
 
 from PIL import Image
 
@@ -18,6 +19,16 @@ def main():
     duration=50,  # 1フレームあたりの表示時間(ms)
     loop=0,  # 0 = 無限ループ
   )
+
+  # PNG形式で保存
+  os.makedirs("out/png", exist_ok=True)
+
+  for i, image in enumerate(images):
+    # PPMファイル名から対応するPNGファイル名を生成
+    ppm_filename = ppm_files[i]
+    png_filename = os.path.basename(ppm_filename).replace(".ppm", ".png")
+    path = os.path.join("out/png", png_filename)
+    image.save(path)
 
 
 if __name__ == "__main__":
